f3aa076f3fbd212a2f32bd87bc6e4aa65b9a489b
Arthur Zamarin <arthurzam@gentoo.org>
2022-11-07 20:37:11 +0200

    bugs_fetcher: skip bugs marked with manual runtime testing
    Resolves: https://github.com/arthurzam/tattoo/issues/1
    Signed-off-by: Arthur Zamarin <arthurzam@gentoo.org>

--- bugs_fetcher.py
+++ bugs_fetcher.py
@@ -1,6 +1,12 @@
 from typing import Iterable, Iterator
 from nattka.bugzilla import BugCategory, BugInfo, NattkaBugzilla
 
+try:
+    from nattka.bugzilla import BugRuntimeTestingState
+    MANUAL_TESTING = BugRuntimeTestingState.MANUAL
+except ImportError:
+    MANUAL_TESTING = 'Manual'
+
 import messages
 
 def read_api_key():
@@ -14,6 +20,8 @@ nattka_bugzilla = NattkaBugzilla(api_key=read_api_key())
 
 def is_ready(bug: BugInfo) -> bool:
     if bug.resolved:
+        return False
+    if getattr(bug, 'runtime_testing_required', None) == MANUAL_TESTING:
         return False
     if len(bug.depends) > 0:
         results = nattka_bugzilla.find_bugs(bugs=bug.depends, unresolved=True)
