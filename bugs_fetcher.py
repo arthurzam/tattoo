b6575003e0335b1881c8139c20d1fb925a7f9510
Arthur Zamarin <arthurzam@gentoo.org>
2024-03-15 17:31:22 +0200

    bugs_fetcher: consider depends if another arch
    When checking depends bug, if unresolved and is stable / keyword, can
    continue if arch is not CCed there.
    
    Resolves: https://gitlab.gentoo.org/arthurzam/tattoo/-/issues/5
    Signed-off-by: Arthur Zamarin <arthurzam@gentoo.org>

--- bugs_fetcher.py
+++ bugs_fetcher.py
@@ -18,22 +18,23 @@ def read_api_key():
 
 nattka_bugzilla = NattkaBugzilla(api_key=read_api_key())
 
-def is_ready(bug: BugInfo) -> bool:
-    if bug.resolved:
-        return False
+def check_bug(bug: BugInfo, depends_bugs: dict[int, BugInfo], worker: messages.Worker) -> bool:
     if getattr(bug, 'runtime_testing_required', None) == MANUAL_TESTING:
         return False
-    if len(bug.depends) > 0:
-        results = nattka_bugzilla.find_bugs(bugs=bug.depends, unresolved=True)
-        if results.items():
-            return False
+    if (bug.category == BugCategory.KEYWORDREQ) != worker.is_rekeyword():
+        return False
+    for dep in bug.depends:
+        if dep := depends_bugs.get(dep):
+            if dep.category not in (BugCategory.KEYWORDREQ, BugCategory.STABLEREQ):
+                # if not another keyword/stable request, then it has a blocker bug
+                return False
+            if not dep.sanity_check:
+                # if the blocker is not ready, then this one is also not ready
+                return False
+            if worker.canonical_arch() in (cc.removesuffix('@gentoo.org') for cc in dep.cc):
+                # if any of the blockers has the arch, then it's not ready
+                return False
     return True
-
-def check_bug(bug: BugInfo, worker: messages.Worker) -> bool:
-    return (
-        (bug.category == BugCategory.KEYWORDREQ) == worker.is_rekeyword() and
-        worker.canonical_arch() in (cc.removesuffix('@gentoo.org') for cc in bug.cc)
-    )
 
 def collect_bugs(bugs_no: Iterable[int], *workers: messages.Worker) -> Iterator[tuple[messages.Worker, list[int]]]:
     bugs = nattka_bugzilla.find_bugs(
@@ -42,7 +43,9 @@ def collect_bugs(bugs_no: Iterable[int], *workers: messages.Worker) -> Iterator[
         sanity_check=[True],
         cc={f'{worker.canonical_arch()}@gentoo.org' for worker in workers},
     )
-    bugs = {bug_no: bug for bug_no, bug in bugs.items() if is_ready(bug)}
+    all_depends = frozenset().union(*(bug.depends for bug in bugs.values()))
+    depends_bugs = nattka_bugzilla.find_bugs(bugs=all_depends, unresolved=True)
+
     for worker in workers:
-        if ok_bugs := [bug_no for bug_no, bug in bugs.items() if check_bug(bug, worker)]:
+        if ok_bugs := [bug_no for bug_no, bug in bugs.items() if check_bug(bug, depends_bugs, worker)]:
             yield worker, ok_bugs
