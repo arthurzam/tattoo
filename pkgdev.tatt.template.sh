arthurzam/tattoo

commit 6ba62500e1937be0ad1d8e59b65a1f06a23ad295

pkgdev.tatt.template.sh: pre-install dependencies for non-tests
Otherwise, we do --getbinpkg=n ... on the only run (unlike for tests)
and can't make use of binpkgs. This was originally added in tatt.git
in eafc7c666d773cfd3b8295474a24cb6eef0919ce because I think --usepkg-exclude
may not work correctly (yet) for --getbinpkg.

Signed-off-by: Sam James <sam@gentoo.org>

pkgdev.tatt.template.sh
@@ -100,6 +100,9 @@ tatt_test_pkg() {
         printf "%s pkgdev_tatt_{{ job_name }}_test\n" "${1}"> "/etc/portage/package.env/pkgdev_tatt_{{ job_name }}/${CP}"
         echo "features: test" >> "{{ report_file }}"
     else
+        # Try to pre-emerge dependencies so that we can make use of getbinpkg
+        # here.
+        tattoo_emerge "${1}" --onlydeps --quiet --oneshot --usepkg-exclude="${name}"
         printf "%s pkgdev_tatt_{{ job_name }}_no_test\n" "${1}" > "/etc/portage/package.env/pkgdev_tatt_{{ job_name }}/${CP}"
         echo "features: " >> "{{ report_file }}"
     fi

commit 38859cfcd369b51e39973e1410d94cd80cde26e7

pkgdev.tatt.template.sh: move `name` assignment earlier
Fixes: 6ba62500e1937be0ad1d8e59b65a1f06a23ad295
Signed-off-by: Sam James <sam@gentoo.org>

pkgdev.tatt.template.sh
@@ -84,6 +84,10 @@ tatt_test_pkg() {
         EMERGE_OUTPUT=/dev/tty
     fi
 
+    # --usepkg-exclude needs the package name, so let's extract it
+    # from the atom we have
+    local name=$( pquery --no-version "${1}" )
+
     # We run some emerge commands twice here with --usepkg=n because of a
     # --with-test-deps quirk (bug #639588).
     if [[ ${2} == "--test" ]]; then
@@ -111,10 +115,6 @@ tatt_test_pkg() {
     {% endfor %}
 
     printf "%s %s\n" "${1}" "${TUSE}" > "/etc/portage/package.use/pkgdev_tatt_{{ job_name }}/${CP}"
-
-    # --usepkg-exclude needs the package name, so let's extract it
-    # from the atom we have
-    local name=$( pquery --no-version "${1}" )
 
     eout=$( tattoo_emerge "${1}" --oneshot --getbinpkg=n --usepkg-exclude="${name}" )
     local RES=$?