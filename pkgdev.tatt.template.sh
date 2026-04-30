b0127be9af748637871539b044139f49f67ae033
Eli Schwartz <eschwartz@gentoo.org>
2026-04-30 11:25:09 -0400

    pkgdev.tatt.template.sh: it is a MASSIVE red flag if pquery returns no name
    Don't just not bother passing --usepkg-exclude.
    
    The issue seems to be pquery is not just an atom reformatter, but
    actually looks up the ebuild, then prints the ebuild's atom. This also
    means it does visibility checks so the package disappears as
    ACCEPT_KEYWORDS does not include it.
    
    --raw forces this unhelpful behavior to go away.
    
    Signed-off-by: Eli Schwartz <eschwartz@gentoo.org>
    Signed-off-by: Arthur Zamarin <arthurzam@gentoo.org>

--- pkgdev.tatt.template.sh
+++ pkgdev.tatt.template.sh
@@ -86,7 +86,7 @@ tatt_test_pkg() {
 
     # --usepkg-exclude needs the package name, so let's extract it
     # from the atom we have
-    local name=$( pquery --no-version "${1}" )
+    local name=$( pquery --raw --no-version "${1}" )
 
     # We run some emerge commands twice here with --usepkg=n because of a
     # --with-test-deps quirk (bug #639588).
@@ -106,7 +106,7 @@ tatt_test_pkg() {
     else
         # Try to pre-emerge dependencies so that we can make use of getbinpkg
         # here.
-        tattoo_emerge "${1}" --onlydeps --with-bdeps=y --quiet --oneshot ${name:+--usepkg-exclude="${name}"}
+        tattoo_emerge "${1}" --onlydeps --with-bdeps=y --quiet --oneshot --usepkg-exclude="${name}"
         printf "%s pkgdev_tatt_{{ job_name }}_no_test\n" "${1}" > "/etc/portage/package.env/pkgdev_tatt_{{ job_name }}/${CP}"
         echo "features: " >> "{{ report_file }}"
     fi
@@ -116,7 +116,7 @@ tatt_test_pkg() {
 
     printf "%s %s\n" "${1}" "${TUSE}" > "/etc/portage/package.use/pkgdev_tatt_{{ job_name }}/${CP}"
 
-    eout=$( tattoo_emerge "${1}" --oneshot --getbinpkg=n ${name:+--usepkg-exclude="${name}"} )
+    eout=$( tattoo_emerge "${1}" --oneshot --getbinpkg=n "${name}" )
     local RES=$?
 
     rm -v -f /etc/portage/package.{env,use}/pkgdev_tatt_{{ job_name }}/${CP}
